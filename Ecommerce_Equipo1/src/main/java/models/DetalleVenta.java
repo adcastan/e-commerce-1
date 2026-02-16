be82d929b5afa6101589223b0ea3a49139e203e9
Adrián Castán <151794030+adcastan@users.noreply.github.com>
2026-02-16 14:52:59 -0700

    detalleVenta

--- Ecommerce_Equipo1/src/main/java/models/DetalleVenta.java
+++ Ecommerce_Equipo1/src/main/java/models/DetalleVenta.java
@@ -20,5 +20,69 @@ public class DetalleVenta {
     int cantidad;
     int precioUnitario;
     int subTotal;
+
+    public DetalleVenta(int idDetalleVenta, Venta idVenta, Libro idLibro, int cantidad, int precioUnitario, int subTotal) {
+        this.idDetalleVenta = idDetalleVenta;
+        this.idVenta = idVenta;
+        this.idLibro = idLibro;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.subTotal = subTotal;
+    }
+
+    public int getIdDetalleVenta() {
+        return idDetalleVenta;
+    }
+
+    public void setIdDetalleVenta(int idDetalleVenta) {
+        this.idDetalleVenta = idDetalleVenta;
+    }
+
+    public Venta getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(Venta idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Libro getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(Libro idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(int precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public int getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(int subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalleVenta{" + "idDetalleVenta=" + idDetalleVenta + ", idVenta=" + idVenta + ", idLibro=" + idLibro + ", cantidad=" + cantidad + ", precioUnitario=" + precioUnitario + ", subTotal=" + subTotal + '}';
+    }
+    
+    
     
 }
